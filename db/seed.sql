cd8da4550e08919c1e1a0470feed682a8c2310c5
huntj0n <hunt.jon19@gmail.com>
2021-07-26 22:42:39 -0600

    updated seed for error handling

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(50),
-    director VARCHAR(50),
-    image VARCHAR(50),
+    title TEXT,
+    director TEXT,
+    image TEXT,
     rating INTEGER
 )
